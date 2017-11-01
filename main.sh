82fe53c14639f8ba9e18d76b43d460aa672eca7c
Ignacio Vaquero Guisasola <i.vaqueroguisasola@gmail.com>
2017-11-01 13:17:30 +0100

    Added hostname and port for ssh connection

--- main.sh
+++ main.sh
@@ -2,14 +2,18 @@
 set -e
 
 read -p "SSID: " ssid
-read -p "Password: " -s password
+read -p "Wifi Password: " -s password
 echo ""
-read -p "hidden [y/n]: " hidden
-
+read -p "network hidden [y/n]: " hidden
 while [ $hidden != "y" -a $hidden != "n" ];do
 	echo "Invalid answer."
 	read -p "hidden [y/n]: " hidden
 done
+
+read -p "resin hostname [default: resin]: " resin_hostname
+read -p "resin port [default: 22222]: " resin_port
+resin_hostname=${resin_hostname:-resin}
+resin_port=${resin_port:-22222}
 
 if [ $hidden = "y" ]; then
 	hidden="yes"
@@ -17,4 +21,4 @@ else
 	hidden="no"
 fi
 
-ssh root@resin.local -p22222 "/usr/bin/nmcli dev wifi connect \"$ssid\" password $password hidden $hidden"
+ssh root@${resin_hostname}.local -p${resin_port} "/usr/bin/nmcli dev wifi connect \"$ssid\" password $password hidden $hidden"
